1649b674d836d1032cbbc975bef7b9cdf51be0f9
Jeus Pichardo <alepbuenrostro20@gmail.com>
2023-02-26 00:09:53 -0600

    Practica1-Practica2

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color.fromARGB(255, 17, 78, 243);
+const kPrimaryLightColor = Color.fromARGB(179, 88, 120, 207);
+
+const double defaultPadding = 16.0;
